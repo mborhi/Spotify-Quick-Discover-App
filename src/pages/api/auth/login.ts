5eae53626ab88a63b7635637650180bc8600b994
Marcell Borhi <mborhi@iu.edu>
2022-07-08 15:58:53 -0400

    update /auth/login to request token with state

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -6,23 +6,30 @@ import endpoints from '../../../../endpoints.config';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     // load env variables
     const client_id = endpoints.ClientID;
-    console.log('client_id', client_id);
-    const client_secret = endpoints.ClientSecret;
     const redirect_uri = endpoints.RedirectURI;
-    console.log('redirect_uri: ', redirect_uri);
+    // get scope and state
     const scope = "streaming \
                 user-read-private \
                 user-read-email";
 
+    const state = generateState(16);
     let params = {
         response_type: 'code',
         client_id: client_id,
         redirect_uri: redirect_uri,
-        scope: scope
+        scope: scope,
+        state: state
     };
-    let url = 'https://accounts.spotify.com/authorize?' + stringify(params);
+    const url = 'https://accounts.spotify.com/authorize?' + stringify(params);
     res.redirect(url);
+}
 
-
+const generateState = (length: number): string => {
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let str = "";
+    for (let i = 0; i < length; i++) {
+        str += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return str;
 }
 
