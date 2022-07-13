3621dbd09fc0d073823a7ad63576b5bad9da662a
Marcell Borhi <mborhi@iu.edu>
2022-07-13 19:05:30 -0400

    update playback pause endpoint error handling

--- src/pages/api/playback/pause.ts
+++ src/pages/api/playback/pause.ts
@@ -1,19 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import endpointsConfig from "../../../../endpoints.config";
-import { queryDatabase } from "../../../../utils/database";
+import { getAccessToken } from "../../../../utils/refreshToken";
 
 // TODO: abstrac this with play.ts
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { refresh_token } = req.headers;
     // handle fetching access_token
-    const { expires_in } = await queryDatabase('authTokens', { refresh_token: refresh_token });
-    // if the access_token is expired, insert an updated entry into database
-    if (Date.now() > parseInt(expires_in.toString())) {
-        await fetch(`http://localhost:3000/api/auth/refresh_token?refresh_token=${refresh_token}`, { method: 'POST' });
+    const access_token = await getAccessToken(refresh_token.toString());
+    // validated access_token
+    if (access_token === undefined) {
+        res.status(401).json({ error: { status: 401, message: "invalid token" } });
+        res.end();
     }
-    // query database for the access_token using the refresh_token
-    const { access_token } = await queryDatabase('authTokens', { refresh_token: refresh_token });
     const { device_id } = req.query;
+    if (!validDeviceId(device_id)) {
+        res.status(400).json({ error: { status: 400, message: "invalid device_id" } })
+    }
     let url = endpointsConfig.SpotifyAPIBaseURL + "/me/player/pause?" + device_id.toString();
     const response = await fetch(url, {
         method: 'PUT',
@@ -24,8 +26,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         }
     });
 
-    res.json({ "message": "pausing song..." });
+    res.json({ "message": "pausing song" });
 
+}
+
+/**
+ * Returns true if the given device_id is a string and is non-empty
+ * @param device_id the device_id to check
+ * @returns whether the device_id is valid
+ */
+const validDeviceId = (device_id: (string | string[])) => {
+    return typeof device_id === 'string' && device_id.length > 0;
 }
 
 export default handler;
